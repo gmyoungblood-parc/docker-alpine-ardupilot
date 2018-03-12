008ee4297d15f5e158f96c60f8cd1b1d714d9549
Eduardo Marques <eduardorbmarques@gmail.com>
2018-03-12 21:15:44 +0000

    Made script more generic

--- scripts/runMAV.sh
+++ scripts/runMAV.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
-if [ $# -ne 2 ] 
+if [ $# -ne 3 ] 
 then
-  echo "Usage: runMAV.sh <system_id> <gcs_port>"
+  echo "Usage: runMAV.sh <system_id> <gcs_host> <gcs_port>"
   exit 1
 fi
 
+SYS_ID=$1
+GCS_HOST=$2
+GCS_PORT=$3
 SCRIPTS_DIR=$(cd $(dirname $(which $0)); pwd)
-docker run --rm -it -v $SCRIPTS_DIR:/external -e "SIM_OPTIONS=--out=udpout:docker.for.mac.localhost:$2 -m --target-system=$1" --entrypoint "/external/entryPoint.sh" gmyoungbloodparc/ardupilot-sitl $1
+
+echo "Running SITL simulation ..."
+echo SYS_ID=$SYS_ID GCS_HOST=$2 GCS_PORT=$3
+
+docker run --rm -it -v $SCRIPTS_DIR:/external -e "SIM_OPTIONS=--out=udpout:$GCS_HOST:$GCS_PORT -m --target-system=$SYS_ID" --entrypoint "/external/entryPoint.sh" gmyoungbloodparc/ardupilot-sitl $SYS_ID
 exit $?
